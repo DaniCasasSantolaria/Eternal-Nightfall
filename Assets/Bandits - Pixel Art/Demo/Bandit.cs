fbb9266e108857cdf4eff6c17ea3227d2d82a016
Daniel Casas <daniel.casas@enti.cat>
2024-06-14 16:35:49 +0200

    A

--- Assets/Bandits - Pixel Art/Demo/Bandit.cs
+++ Assets/Bandits - Pixel Art/Demo/Bandit.cs
@@ -1,37 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bandit : MonoBehaviour {
+public class Bandit : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_coyoteTime = 0.2f; // Tiempo de gracia para saltar después de dejar el suelo
+    [SerializeField] float m_jumpBufferTime = 0.2f; // Tiempo de buffer para registrar el intento de salto
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_Bandit       m_groundSensor;
-    private bool                m_grounded = false;
-    private bool                m_combatIdle = false;
-    private bool                m_isDead = false;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_Bandit m_groundSensor;
+    private bool m_grounded = false;
+    private bool m_combatIdle = false;
+    private bool m_isDead = false;
+    private float m_coyoteTimeCounter;
+    private float m_jumpBufferCounter;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State()) {
+        if (!m_grounded && m_groundSensor.State())
+        {
             m_grounded = true;
+            m_coyoteTimeCounter = m_coyoteTime;
             m_animator.SetBool("Grounded", m_grounded);
         }
 
         //Check if character just started falling
-        if(m_grounded && !m_groundSensor.State()) {
+        if (m_grounded && !m_groundSensor.State())
+        {
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        // Decrease coyote time counter
+        if (!m_grounded)
+        {
+            m_coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        // Decrease jump buffer counter
+        if (m_jumpBufferCounter > 0)
+        {
+            m_jumpBufferCounter -= Time.deltaTime;
         }
 
         // -- Handle input and movement --
@@ -51,21 +73,23 @@ public class Bandit : MonoBehaviour {
 
         // -- Handle Animations --
         //Death
-        if (Input.GetKeyDown("e")) {
-            if(!m_isDead)
+        if (Input.GetKeyDown("e"))
+        {
+            if (!m_isDead)
                 m_animator.SetTrigger("Death");
             else
                 m_animator.SetTrigger("Recover");
 
             m_isDead = !m_isDead;
         }
-            
+
         //Hurt
         else if (Input.GetKeyDown("q"))
             m_animator.SetTrigger("Hurt");
 
         //Attack
-        else if(Input.GetMouseButtonDown(0)) {
+        else if (Input.GetMouseButtonDown(0))
+        {
             m_animator.SetTrigger("Attack");
         }
 
@@ -73,13 +97,21 @@ public class Bandit : MonoBehaviour {
         else if (Input.GetKeyDown("f"))
             m_combatIdle = !m_combatIdle;
 
-        //Jump
-        else if (Input.GetKeyDown("space") && m_grounded) {
+        // Register jump input with buffer time
+        if (Input.GetKeyDown("space"))
+        {
+            m_jumpBufferCounter = m_jumpBufferTime;
+        }
+
+        // Jump
+        if (m_jumpBufferCounter > 0 && m_coyoteTimeCounter > 0)
+        {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
             m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
             m_groundSensor.Disable(0.2f);
+            m_jumpBufferCounter = 0;
         }
 
         //Run
